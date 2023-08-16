d90f2379dd2a5dabe7b94d491d0bda1b7ac2aa25
Anurag-996 <anuragvishwakarma546@gmail.com>
2023-08-16 20:57:40 +0530

    pizza bill generator

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,9 +5,17 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private Boolean extraCheeseAdded;
+    private Boolean extraToppingsAdded;
+    private Boolean takeawayAdded;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        this.price = isVeg ? 300 : 400;
+        this.extraCheeseAdded = false;
+        this.extraToppingsAdded = false;
+        this.takeawayAdded = false;
+        this.bill = "";
         // your code goes here
     }
 
@@ -17,14 +25,29 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (!extraCheeseAdded) {
+            this.price += 80;
+            this.extraCheeseAdded = true;
+            this.bill += "Extra Cheese Added: 80\n";
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!extraToppingsAdded) {
+            this.price += (isVeg ? 70 : 120);
+            this.extraToppingsAdded = true;
+            this.bill += "Extra Toppings Added: " + (isVeg ? 70 : 120) + "\n";
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!takeawayAdded) {
+            this.price += 20;
+            this.takeawayAdded = true;
+            this.bill += "Paperbag Added: 20\n";
+        }
     }
 
     public String getBill(){
